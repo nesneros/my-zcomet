db5694c64dfd6dac67f62be4a84aea9125a2314f
Jan Sorensen <jsorensen@blockdaemon.com>
2023-04-13 09:26:32 +0200

    Fix zcomet update

--- init.zsh
+++ init.zsh
@@ -22,7 +22,7 @@ zcomet load ohmyzsh/ohmyzsh lib completion.zsh directories.zsh functions.zsh his
 
 # Load lib folder
 for config_file ($MY_ZCOMET/lib/*.zsh(N)); do
-  source $config_file
+    source $config_file
 done
 unset config_file
 
@@ -62,9 +62,36 @@ for dir in $path; do
 done
 path=("${valid_dirs[@]}")
 
+
+# Autoload 
+autoload -Uz upgradeall
+
 zcomet compinit
 
 # Create aliases that changes system commands after compinit
+
+alias ll="ls -l"
+alias week="date +%V"
+
+alias+ pgrep -fli
+alias+ df -h
+
+# Some global aliases
+alias -g B='| bat'
+alias -g F='| less +F'
+alias -g GREP='| grep --ignore-case -E'
+alias -g G='| rg --smart-case'
+alias -g H='| head'
+alias -g L='| less'
+alias -g S='| sort'
+alias -g T='| tail -100f'
+alias -g P='| peco'
+alias -g Z='| fzf --tac'
+# Colorize json
+alias -g J='|jq .'
+
+alias myip='echo $(curl -s http://whatismyip.akamai.com/)'
+# alias myip='dig +short myip.opendns.com @resolver1.opendns.com'
 
 # Disable glob for some commands
 for e in find fd mdfind rsync scp; do
