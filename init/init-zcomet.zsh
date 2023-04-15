4413194d03fe26d45ded1977bb598e67c1704eb2
Jan Sorensen <jsorensen@blockdaemon.com>
2023-04-15 09:55:19 +0200

    Init zcomet from init folder

--- init/init-zcomet.zsh
+++ init/init-zcomet.zsh
@@ -1,4 +1,25 @@
+# Source this from your .zshrc file to load zcomet.
+# If zcomet is not installed it will be cloned.
+# Then continue to load this as a zcomet plugin
+
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-source ${0:h}/bootstrap.zsh
+# Setup MY_ZCOMET to point to this plugin
+if [[ -z "$MY_ZCOMET" ]]; then
+  MY_ZCOMET=${0:h:h}
+elif [[ "$MY_ZCOMET" != "${0:h:h}" ]]; then
+  echo "MY_ZCOMET mismatch: ${MY_ZCOMET} != ${0:h:h}"
+  read && return 1
+fi
+
+if [[ ! -f ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh ]]; then
+  command git clone https://github.com/agkozak/zcomet.git ${ZDOTDIR:-${HOME}}/.zcomet/bin
+fi
+source ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh
+
+# Make the ZCOMET_SOURCE variable available to all scripts so it can be sourced to make zcomet available
+export ZCOMET_SOURCE=$ZCOMET[SCRIPT]
+export MY_ZCOMET
+
+zcomet load "$MY_ZCOMET"
