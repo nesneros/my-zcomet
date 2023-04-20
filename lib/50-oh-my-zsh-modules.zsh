f7cc991bea01ce3c2e628c0cdcc41d5747adbe2f
Jan Sorensen <jsorensen@blockdaemon.com>
2023-04-20 10:34:13 +0200

    Docker completion from OMZ

--- lib/50-oh-my-zsh-modules.zsh
+++ lib/50-oh-my-zsh-modules.zsh
@@ -1,6 +1,9 @@
 # Include some standard functionality form OMZ
-zcomet load ohmyzsh/ohmyzsh lib completion.zsh directories.zsh functions.zsh history.zsh key-bindings.zsh misc.zsh
+zcomet load ohmyzsh lib completion.zsh directories.zsh functions.zsh history.zsh key-bindings.zsh misc.zsh
 
 # Load some OMZ plugins
 zcomet load ohmyzsh plugins/dirhistory
 zcomet load ohmyzsh plugins/macos
+
+# Docker completion
+zcomet fpath ohmyzsh plugins/docker
