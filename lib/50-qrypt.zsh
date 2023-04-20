761281be8b91319731553216bd41a2a9c93a3433
Jan Sorensen <jsorensen@blockdaemon.com>
2023-04-20 14:55:33 +0200

    Simplify lib/qrypt

--- lib/50-qrypt.zsh
+++ lib/50-qrypt.zsh
@@ -1,6 +1,2 @@
-searchDirs=($HOME $HOME/Documents)
-for dir in $searchDirs; do
-  if [ -d "$dir" ] ; then
-    [ -z "$QRYPT_DIR" ] && [ -d "$dir/.qrypt" ] && export QRYPT_DIR="$dir/.qrypt"
-  fi
-done
+local dirs=({$HOME,$HOME/Documents}/.qrypt(/N))
+[[ -z "$dirs" ]] || export QRYPT_DIR="$dirs[1]"
