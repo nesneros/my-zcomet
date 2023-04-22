f77b98691f2cf9ab21905f4c1ea55759d3a5ba2f
Jan Hvid Sørensen <sorensenjan@gmail.com>
2023-04-22 08:09:59 +0200

    Move autoloads to autoloads folder

--- init.zsh
+++ init.zsh
@@ -2,10 +2,10 @@
 export ZCOMET_SOURCE=$ZCOMET[SCRIPT]
 export MY_ZCOMET
 
+autoload -Uz $MY_ZCOMET/autoloads/*
+
 # Load lib folder
 for config_file ($MY_ZCOMET/lib/*.zsh); do
     source $config_file
 done
 unset config_file
-
-autoload -Uz $MY_ZCOMET/functions/*
