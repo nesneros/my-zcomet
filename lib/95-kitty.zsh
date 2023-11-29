b30e3acb28329ec704ce3733c3e24cf88abb28f6
Jan Sorensen <sorensenjan@gmail.com>
2023-11-29 20:47:50 +0100

    Merge

--- lib/95-kitty.zsh
+++ lib/95-kitty.zsh
@@ -3,4 +3,10 @@ if (( ! ${+commands[kitty]} )); then
 fi
 
 alias icat="kitty +kitten icat"
-alias kitty-close-other-tabs="for id in \$(kitty @ ls | jq '.[0].tabs | .[] | select(.is_focused == false) | .id'); do kitty @ close-tab --match=id:\$id; done"
+alias kt-close-other-tabs="for id in \$(kitty @ ls | jq '.[0].tabs | .[] | select(.is_focused == false) | .id'); do kitty @ close-tab --match=id:\$id; done"
+
+# Some alias to work with multiple Kitty windows
+alias kt-tab-cd="kitty-send --command tab cd
+alias kt-window-cd="kitty-send --command osWindow cd
+alias kt-tab-pwd-cd="kitty-send --command --skip-me tab cd \$(pwd)"
+alias kt-window-pwd-cd="kitty-send --command --skip-me osWindow cd \$(pwd)"
