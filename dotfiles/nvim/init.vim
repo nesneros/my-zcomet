a47041b0f8480c0eace81056240b4dc36c517aa5
Jan Hvid Sørensen <sorensenjan@gmail.com>
2024-07-14 21:46:06 +0200

    Add neovim configuration and vi-mode plugin for enhanced editing.

---
+++ dotfiles/nvim/init.vim
@@ -0,0 +1,8 @@
+set nocompatible
+
+lua require("nesneros")
+
+set number
+set relativenumber
+set autoindent
+set backspace=indent,eol,start
---
+++ lib/99-vim-mode.zsh
@@ -0,0 +1,10 @@
+zcomet load ohmyzsh plugins/vi-mode
+
+# Yank to the system clipboard
+function vi-yank-xclip {
+    zle vi-yank
+    echo "$CUTBUFFER" | pbcopy -i
+}
+
+zle -N vi-yank-xclip
+bindkey -M vicmd 'y' vi-yank-xclip
