398b98060c93dc0ab289a26ffc93ccf8f4832578
Jan Hvid Sørensen <sorensenjan@gmail.com>
2024-07-14 09:16:03 +0200

    remove -x

--- setup/krew.sh
+++ setup/krew.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
-set -ex
+set -e
+
 cd "$(mktemp -d)"
 OS="$(uname | tr '[:upper:]' '[:lower:]')"
 ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')"
