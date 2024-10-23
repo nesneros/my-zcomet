9ce14f3abbe5ee02be9386c62c27a6768db531eb
Jan Hvid Sørensen <sorensenjan@gmail.com>
2024-10-23 18:47:51 +0200

    Remove karabiner

--- other/karabiner/README.md
+++
@@ -1,2 +0,0 @@
-* Copy Karabiner mappings to $HOME/.config/karabiner/assets/complex_modifications
-* Enable the rules in Karabiner UI
--- other/karabiner/copy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-mkdir -p ~/.config/karabiner/assets/complex_modifications
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cp -v "$DIR/my-zcomet-rules.json" ~/.config/karabiner/assets/complex_modifications
