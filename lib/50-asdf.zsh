13bbd258bb8b71321563ae216770671b6873218f
Jan Hvid Sørensen <sorensenjan@gmail.com>
2025-02-09 21:38:16 +0100

    For asdf 0.16

--- lib/50-asdf.zsh
+++ lib/50-asdf.zsh
@@ -2,6 +2,9 @@ if (( ! ${+commands[asdf]} )); then
   return
 fi
 
+export ASDF_DATA_DIR="$HOME/.asdf"
+export PATH="$ASDF_DATA_DIR/shims:$PATH"
+
 # this should be default for ASDF_DEFAULT_TOOL_VERSIONS_FILENAME but explicit setting it removes from errors. Eg for asdf direnv envrc
 export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME=.tool-versions
 export ASDF_CONFIG_FILE=$MY_ZCOMET/dotfiles/asdfrc
