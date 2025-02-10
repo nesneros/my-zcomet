4c7ab35900c183c9e8a42cdc840857742b41a050
Jan Sorensen <sorensenjan@gmail.com>
2025-02-10 06:43:36 +0100

    Add kt-detach alias

--- lib/95-kitty.zsh
+++ lib/95-kitty.zsh
@@ -10,3 +10,5 @@ alias kt-tab-cd="kitty-send --command tab cd
 alias kt-window-cd="kitty-send --command osWindow cd
 alias kt-tab-pwd-cd="kitty-send --command --skip-me tab cd \$(pwd)"
 alias kt-window-pwd-cd="kitty-send --command --skip-me osWindow cd \$(pwd)"
+
+alias kt-detach="kitty @ detach-tab --self"
