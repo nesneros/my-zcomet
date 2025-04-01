df10109af10d092453e632d2f000344dbab9f45c
Jan Sorensen <sorensenjan@gmail.com>
2025-04-01 07:12:50 +0200

    Set env vars for mise

--- lib/50-mise.zsh
+++ lib/50-mise.zsh
@@ -1,5 +1,9 @@
 hash mise &>/dev/null || return
 
-eval "$(mise activate zsh)"
+# If ~/etc/age.txt exists, use it as the SOPS_AGE_KEY_FILE. These env vars should be set before activating mise.
+if [[ -f "$HOME/.config/mise/age.txt" ]]; then
+    export SOPS_AGE_KEY_FILE="$HOME/.config/mise/age.txt"
+    export MISE_SOPS_AGE_KEY_FILE="$HOME/.config/mise/age.txt"
+fi
 
-export SOPS_AGE_KEY_FILE="$HOME/.config/mise/age.txt"
+eval "$(mise activate zsh)"
