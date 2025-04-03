e329c5ac8aa63bfdc6a62aa8c23fe49ba7f90071
Jan Hvid Sørensen <sorensenjan@gmail.com>
2025-04-03 04:55:01 +0200

    posh cleanup

--- lib/99-oh-my-posh.zsh
+++ lib/99-oh-my-posh.zsh
@@ -1,7 +1,3 @@
 hash oh-my-posh  &>/dev/null || return
 
-# OH_MY_POSH_THEME=powerlevel10k_modern
-
-#eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/$OH_MY_POSH_THEME.omp.json)" # --config $MY_ZCOMET/dotfiles/oh-my-posh/newline.json)"
-
 eval "$(oh-my-posh init zsh --config $MY_ZCOMET/dotfiles/oh-my-posh.json)"
