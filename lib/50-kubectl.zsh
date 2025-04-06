44a3ff4bae77463f9e9256394a65deefc163e67a
Jan Hvid Sørensen <sorensenjan@gmail.com>
2025-04-06 17:38:10 +0200

    Remove kc alias

--- lib/50-kubectl.zsh
+++ lib/50-kubectl.zsh
@@ -1,7 +1,6 @@
 (( $+commands[kubectl] )) || return
     
 alias k=kubectl
-alias kc=kubectl
 
 # End of setup path will be cleaned up so just add to path without check
 export PATH="${HOME}/.krew/bin:$PATH"
