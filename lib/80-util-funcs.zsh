6f834ca0b6758ebdb23d1de158baece42f2672bd
Jan Hvid Sørensen <sorensenjan@gmail.com>
2025-04-22 01:05:33 +0200

    --usage: Define util-funcs.zsh

---
+++ lib/80-util-funcs.zsh
@@ -0,0 +1,16 @@
+# from http://chneukirchen.org/blog/archive/2013/03/10-fresh-zsh-tricks-you-may-not-know.html
+imv() {
+  local src dst
+  for src; do
+    [[ -e $src ]] || { print -u2 "$src does not exist"; continue }
+    dst=$src
+    vared dst
+    [[ $src != $dst ]] && mkdir -p $dst:h && mv -n $src $dst
+  done
+}
+
+# Convenient function to create a dir and cd into it
+mcd() {
+    mkdir -p "$1"
+    cd "$1"
+}
