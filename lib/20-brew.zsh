b633423e417a07111e1622b37ff85c0a9e74bba9
Jan Sorensen <sorensenjan@gmail.com>
2025-04-26 11:00:10 +0200

    fix brew init

--- lib/20-brew.zsh
+++ lib/20-brew.zsh
@@ -1,10 +1,10 @@
-# if [[ -z "$HOMEBREW_PREFIX" ]] ; then
-#   local brewBins=({/opt/homebrew,/usr/local,/home/linuxbrew/.linuxbrew}/bin/brew(.N))
-#   local brewBin=${brewBins[1]}
-#   if [[ -x "$brewBin" ]] ; then
-#     eval $($brewBin shellenv)
-#   fi
-# fi
+if [[ -z "$HOMEBREW_PREFIX" ]] ; then
+  local brewBins=({/opt/homebrew,/usr/local,/home/linuxbrew/.linuxbrew}/bin/brew(.N))
+  local brewBin=${brewBins[1]}
+  if [[ -x "$brewBin" ]] ; then
+    eval $($brewBin shellenv)
+  fi
+fi
 
 if (( ${+commands[brew]} )) ; then
   [[ -z "$HOMEBREW_PREFIX" ]] && echo "HOMEBREW_PREFIX not defined"
