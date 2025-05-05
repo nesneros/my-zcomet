897f2a59b57cbec1ce2c95296d6da6b6e1469e17
Jan Hvid Sørensen <sorensenjan@gmail.com>
2025-05-05 04:11:06 +0200

    Include brew install comment

--- lib/50-yazi.zsh
+++ lib/50-yazi.zsh
@@ -1,5 +1,8 @@
 hash yazi &>/dev/null || return
 
+# Yazi uses a number of tools. All can be installed via brew:
+# brew install yazi ffmpeg sevenzip jq poppler fd ripgrep fzf zoxide resvg imagemagick font-symbols-only-nerd-font
+
 function y() {
 	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
 	yazi "$@" --cwd-file="$tmp"
