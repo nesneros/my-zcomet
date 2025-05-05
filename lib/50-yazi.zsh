a0fab0f37e09fb4bdf2cf4c74803f8b4459e07e7
Jan Hvid Sørensen <sorensenjan@gmail.com>
2025-05-05 04:07:02 +0200

    --usage: support yazi

---
+++ lib/50-yazi.zsh
@@ -0,0 +1,10 @@
+hash yazi &>/dev/null || return
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
