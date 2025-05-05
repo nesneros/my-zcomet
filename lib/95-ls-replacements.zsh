04c3ad3c2ea9d488af1639cad70771aa91905a45
Jan Hvid Sørensen <sorensenjan@gmail.com>
2025-05-05 04:07:14 +0200

    --usage: Fix ls replacements

--- lib/95-ls-replacements.zsh
+++ lib/95-ls-replacements.zsh
@@ -2,7 +2,7 @@ if (( ${+commands[lsd]} )) ; then
     alias+ lsd -hA --group-dirs first 
 fi
 
-if (( ${+commands[exa]} )) ; then
+if (( ${+commands[eza]} )) ; then
     alias+ eza --git -a --icons --group-directories-first --ignore-glob=".git\|.DS_Store"
     # Change color of date column (da), git modified (gm) in long listings
     export EZA_COLORS="da=37:gm=31:di=36"
