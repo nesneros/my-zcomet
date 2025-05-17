b6d68d1846f08fad887dee2f586daff5338a97ea
Jan Hvid Sørensen <sorensenjan@gmail.com>
2025-05-17 15:48:09 +0200

    Define XDG_cONFIG_HOME

--- init.zsh
+++ init.zsh
@@ -1,6 +1,7 @@
 # Make the ZCOMET_SOURCE variable available to all scripts so it can be sourced to make zcomet available
 export ZCOMET_SOURCE=$ZCOMET[SCRIPT]
 export MY_ZCOMET
+export XDG_CONFIG_HOME="$HOME/.config"
 export PATH=$MY_ZCOMET/bin:$HOME/share/bin:$HOME/bin:$PATH
 export FPATH=$MY_ZCOMET/generated/functions:$FPATH
 
