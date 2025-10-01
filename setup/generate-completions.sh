ab3177ac502b8a58e1990f1710bf0f521ece6580
Jan Sorensen <sorensenjan@gmail.com>
2025-10-01 13:23:08 +0200

    Add mise to completions

--- setup/generate-completions.sh
+++ setup/generate-completions.sh
@@ -17,5 +17,6 @@ _gen-comp "$destDir" golangci-lint
 _gen-comp "$destDir" jira
 _gen-comp "$destDir" kubectl
 _gen-comp "$destDir" orbctl
+_gen-comp "$destDir" mise
 _gen-comp "$destDir" ov --completion
 # _gen-comp "$destDir" usage --completions
