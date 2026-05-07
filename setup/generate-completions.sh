e602241cbadc8b01e6bdb8c13c6da17db3cab84a
Jan Sorensen <sorensenjan@gmail.com>
2026-05-07 13:17:20 +0200

    Add k3d to completions

--- setup/generate-completions.sh
+++ setup/generate-completions.sh
@@ -14,6 +14,7 @@ destDir="$MY_ZCOMET/generated/functions"
 mkdir -p "$destDir"
 _gen-comp "$destDir" golangci-lint
 _gen-comp "$destDir" jira
+_gen-comp "$destDir" k3d
 _gen-comp "$destDir" kubectl
 _gen-comp "$destDir" orbctl
 _gen-comp "$destDir" ov --completion
